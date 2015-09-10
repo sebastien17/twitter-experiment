6d772f674f2ac6920a85821bf86b444eda5ea4f6
mirkolai <mirko.lai.1987@gmail.com>
2015-09-10 13:57:26 +0200

    Create  statutes-show.py
    Returns a single Tweet, specified by the id parameter. The Tweet’s author will also be embedded within the tweet.
    https://dev.twitter.com/rest/reference/get/statuses/show/%3Aid

---
+++ statutes-show.py
@@ -0,0 +1,34 @@
+__author__ = 'mirko'
+import oauth2 as oauth
+import time
+import json
+
+CONSUMER_KEY    = ""
+CONSUMER_SECRET = ""
+ACCESS_KEY      = ""
+ACCESS_SECRET   = ""
+
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
+
+ids = ["20"]
+
+for id in ids:
+
+    place_endpoint = "https://api.twitter.com/1.1/statuses/show.json?id="+id
+    response, data = client.request(place_endpoint)
+    print response
+    print response['status']
+    print response['x-rate-limit-limit']
+    print data
+    if response['status']=='200':
+        if int(response['x-rate-limit-remaining'])<2:
+            print 'id rescue: wait '+str( int(response['x-rate-limit-reset']) - int(time.time()) )+' seconds'
+            time.sleep(int(response['x-rate-limit-reset'])-int(time.time()))
+
+    jsonTweet=json.loads(data)
+    print json.dumps(jsonTweet,2)
+    print 'id rescue: wait '+str((15*60)/int(response['x-rate-limit-limit']))+' seconds'
+    time.sleep((15*60)/int(response['x-rate-limit-limit']))
